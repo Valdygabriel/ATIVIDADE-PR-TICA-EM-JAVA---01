3289545d28fc2e1869d279a4e4ffc007e576b2ee
Valdy Gabriel <valdygabriel568@gmail.com>
2024-02-29 16:54:27 -0300

    primeiro commit

---
+++ atvpratica1/src/Trabalho/Pessoa.java
@@ -0,0 +1,30 @@
+package Trabalho;
+
+public class Pessoa {
+
+    public String nome = "Marcelo";
+    public String sobrenome = "Gomes";
+    public String dataDeNascimento = "11/05/1978";
+    public String email = "MarceloGomes567@gmail.com";
+    public int telefone = 986534957;
+
+    public void andar() {
+
+    }
+
+    public void comer() {
+
+    }
+
+    public void Correr() {
+
+    }
+
+    public void Dormir() {
+
+    }
+
+    static final String CPF = "21358693740";
+    static final String PAIS_DE_NACIONALIDADE = "Brasil";
+
+}
---
+++ atvpratica1/src/Trabalho/Teste1.java
@@ -0,0 +1,13 @@
+package Trabalho;
+
+public class Teste1 {
+  public static void main(String[] args) {
+    Pessoa Pessoa = new Pessoa();
+
+    System.out.println(Pessoa.nome + " " + Pessoa.sobrenome);
+
+    System.out.println(Pessoa.email + "-" + Pessoa.dataDeNascimento + "-" + Pessoa.telefone);
+
+    System.out.println(Pessoa.PAIS_DE_NACIONALIDADE);
+  }
+}
